591a70785276da9b5d20fba76e314f24412dbde9
Elosowaldo28 <osowal28@gmail.com>
2026-06-03 18:24:30 -0500

    Implementada vinculacion de cuenta con email

--- start/app/src/main/java/com/example/makeitso/model/service/impl/AccountServiceImpl.kt
+++ start/app/src/main/java/com/example/makeitso/model/service/impl/AccountServiceImpl.kt
@@ -57,8 +57,19 @@ class AccountServiceImpl @Inject constructor(private val auth: FirebaseAuth) : A
     auth.signInAnonymously().await()
   }
 
-  override suspend fun linkAccount(email: String, password: String) {
-    //TODO
+  override suspend fun linkAccount(
+    email: String,
+    password: String
+  ) {
+    val credential =
+      EmailAuthProvider.getCredential(
+        email,
+        password
+      )
+
+    auth.currentUser!!
+      .linkWithCredential(credential)
+      .await()
   }
 
   override suspend fun deleteAccount() {
